62dc1cd06d2df5f4f484cca334459005b3036050
mcalus3 <marek-calus@wp.pl>
2020-02-01 15:07:03 +0100

    Set stable package versions, fix button focus bug

--- src/ColorWars/ReactView/components/NavBar.tsx
+++ src/ColorWars/ReactView/components/NavBar.tsx
@@ -1,9 +1,9 @@
-import 'rc-slider/assets/index.css';
-import '../../Settings.css';
-import * as actions from '../../ReduxStore/actionTypes';
+import "rc-slider/assets/index.css";
+import "../../Settings.css";
+import * as actions from "../../ReduxStore/actionTypes";
 
-import * as React from 'react';
-import { Button } from 'react-bootstrap';
+import * as React from "react";
+import { Button } from "react-bootstrap";
 
 export interface Props {
   visible: boolean;
@@ -11,11 +11,26 @@ export interface Props {
 }
 
 class NavBar extends React.Component<Props, object> {
+  handleClick(e: any) {
+    if (e) {
+      e.preventDefault();
+    }
+  }
+
   render() {
     return (
       <div className="NavBar">
-        <Button bsStyle="primary" onClick={this.onShow}>
-          {this.props.visible ? 'Hide settings' : 'Show settings'}
+        <Button
+          bsStyle="primary"
+          onClick={this.onShow}
+          onMouseDown={this.handleClick}
+          onKeyUp={e => {
+            if (e.keyCode === 13 || e.keyCode === 32) {
+              this.handleClick(e);
+            }
+          }}
+        >
+          {this.props.visible ? "Hide settings" : "Show settings"}
         </Button>
       </div>
     );
